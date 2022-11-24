37640a6d05acbeb150aa97eee9e68d89f47d5cf1
TrueGre3n <oeyraymon4@gmail.com>
2022-11-24 17:47:16 +0700

    Ubah form login

--- 160421029_Nico Victorio/160421029_Nico Victorio/FormLogin.Designer.cs
+++ 160421029_Nico Victorio/160421029_Nico Victorio/FormLogin.Designer.cs
@@ -35,6 +35,7 @@ namespace _160421029_Nico_Victorio
             this.tb_Username = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_Exit
@@ -90,11 +91,21 @@ namespace _160421029_Nico_Victorio
             this.label1.TabIndex = 6;
             this.label1.Text = "Username";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(349, 223);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_Exit);
             this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.tb_Password);
@@ -116,5 +127,6 @@ namespace _160421029_Nico_Victorio
         private System.Windows.Forms.TextBox tb_Username;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
